d581d9a3f80b7fb49b4906d3cd082bf5c35c2b44
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-03-24 18:08:04 +0100

    Creacion del Main

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import clases.Ataque
+import clases.Pokemon
+import clases.Tipo
+
+fun main() {
+    // Creación de los Pokemon
+    var Pikachu = Pokemon("Pikachu", "Es amarillo y flama, lo oconoce hasta mi abuela", Tipo("electrico"), 50, 125)
+    var Charmander = Pokemon("Charmander", "Si lo tocas puedes quemarte", Tipo("fuego"), 65, 110)
+    var Squirtle = Pokemon("Squirtle", "Tiene mas agia que el mar", Tipo("agua"), 52, 120)
+
+    //Creación De los Ataques
+    var Pistola_Agua = Ataque("Pistola Agua", Tipo("agua"), 25)
+
+}
