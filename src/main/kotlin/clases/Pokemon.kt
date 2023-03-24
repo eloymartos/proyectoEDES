7c437393850e69f0c796b1fb2d56ae6d43fddc92
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-03-24 17:34:28 +0100

    Resolucion Funcionalidad 1

--- src/main/kotlin/clases/Pokemon.kt
+++ src/main/kotlin/clases/Pokemon.kt
@@ -16,4 +16,7 @@ class Pokemon(nombre:String, lore:String, tipo:Tipo, fuerza:Int, vida:Int) {
         this.vida = vida
     }
 
+    fun recibir_ataque(ataque: Ataque) {
+        vida -= ataque.danio*tipo.efectividad(ataque.tipo).toInt()
+    }
 }
