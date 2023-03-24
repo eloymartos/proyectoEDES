5a75fcb168be51d1ded5c277ac21a956906a5f84
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-03-24 19:23:26 +0100

    provisional del main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import clases.Ataque
+import clases.Jugador
 import clases.Pokemon
 import clases.Tipo
 
@@ -8,7 +9,13 @@ fun main(args:Array<String>) {
     var Charmander = Pokemon("Charmander", "Si lo tocas puedes quemarte", Tipo("fuego"), 65, 110)
     var Squirtle = Pokemon("Squirtle", "Tiene mas agia que el mar", Tipo("agua"), 52, 120)
 
-    //Creación De los Ataques
-    var Pistola_Agua = Ataque("Pistola Agua", Tipo("agua"), 25)
+
+    println("Qué pokemon quieres que tenga entrenador 1?")
+    var opcion1 = readln()
+    var entrenador1 = if (opcion1 == "pikachu") Pikachu else if (opcion1 == "charmander") Charmander else Squirtle
+
+    println("Qué pokemon quieres que tenga entrenador 2?")
+    var opcion2 = readln()
+    var entrenador2 = if (opcion2 == "pikachu") Pikachu else if (opcion2 == "charmander") Charmander else Squirtle
 
 }
