6332dba22d6e1b19e9084f66c2a47667c3c58c0c
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-03-25 17:13:17 +0100

    actuzalizcion

--- src/main/kotlin/clases/Ataque.kt
+++ src/main/kotlin/clases/Ataque.kt
@@ -1,5 +1,14 @@
 package clases
-
+/**
+ *### Clase [Ataque]
+ *
+ * @constructor (nombre: [Strinf], tipo: [Tipo], danio[Int])
+ *
+ * @param nombre [String] : Nombre del ataque en formato Cadena
+ * @param tipo [Tipo] : Es el tipo del ataque y es de formato Tipo que es otra clase del
+ *                      proyecto la cual indica el tipo del pokemon en una cadena y comprueba que sea correcta.
+ * @param danio [Int] : Es el daño que hace cada ataque independientemente de la efectividad y está en formato de número entero
+ */
 class Ataque(nombre: String, tipo:Tipo, danio:Int) {
 
     var nombre : String
--- src/main/kotlin/clases/Jugador.kt
+++ src/main/kotlin/clases/Jugador.kt
@@ -1,5 +1,11 @@
 package clases
-
+/**
+ * ### Clase [Jugador]
+ *
+ * @constructor (pokemon: [Pokemon])
+ *
+ * @param pokemon [Pokemon] : Cada Jugador tiene un pokemon de la clase [Pokemon]
+ */
 class Jugador(pokemon : Pokemon) {
 
     var pokemon:Pokemon
